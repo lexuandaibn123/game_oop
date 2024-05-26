1e1ba50167abd71c4405167a22def04947f4a8eb
duongduke <duongduke1708@gmail.com>
2024-05-26 11:56:24 +0700

    them dungeon, them nhac va hieu hung chuyen doi giua cac the gioi

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -17,9 +17,9 @@ public class Sound {
 	FloatControl fc;
 	int volumeScale = 3;
 	float volume;
-	
+
 	public Sound() {
-		
+
 		soundURL[0] = getClass().getResource("/sound/BlueBoyAdventure.wav");
 		soundURL[1] = getClass().getResource("/sound/coin.wav");
 		soundURL[2] = getClass().getResource("/sound/powerup.wav");
@@ -27,21 +27,23 @@ public class Sound {
 		soundURL[4] = getClass().getResource("/sound/fanfare.wav");
 		soundURL[5] = getClass().getResource("/sound/hitmonster.wav");
 		soundURL[6] = getClass().getResource("/sound/receivedamage.wav");
-//		soundURL[7] = getClass().getResource("/sound/Recording.m4a");
+		// soundURL[7] = getClass().getResource("/sound/Recording.m4a");
 		soundURL[8] = getClass().getResource("/sound/levelup.wav");
 		soundURL[9] = getClass().getResource("/sound/cursor.wav");
 		soundURL[10] = getClass().getResource("/sound/burning.wav");
 		soundURL[11] = getClass().getResource("/sound/cuttree.wav");
 		soundURL[12] = getClass().getResource("/sound/gameover.wav");
 		soundURL[13] = getClass().getResource("/sound/stairs.wav");
+		soundURL[14] = getClass().getResource("/sound/Dungeon.wav");
+		soundURL[15] = getClass().getResource("/sound/Merchant.wav");
 		setFile(0);
 
 	}
-	
+
 	public void setFile(int i) {
-		
+
 		try {
-			
+
 			AudioInputStream ais = null;
 			try {
 				ais = AudioSystem.getAudioInputStream(soundURL[i]);
@@ -58,38 +60,52 @@ public class Sound {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			fc = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+			fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 			checkVolume();
-			
-		} catch(IOException e) {
+
+		} catch (IOException e) {
 
 		}
-		
+
 	}
+
 	public void play() {
-		
+
 		clip.start();
 	}
-	
+
 	public void loop() {
-		
+
 		clip.loop(Clip.LOOP_CONTINUOUSLY);
 	}
-	
+
 	public void stop() {
-		
+
 		clip.stop();
 	}
+
 	public void checkVolume() {
-		switch(volumeScale) {
-		
-		case 0:volume = -80f; break;
-		case 1: volume = -20f; break;
-		case 2: volume = -12f;break;
-		case 3: volume = -5f; break;
-		case 4: volume  = 1f; break;
-		case 5: volume = 6f; break;
+		switch (volumeScale) {
+
+			case 0:
+				volume = -80f;
+				break;
+			case 1:
+				volume = -20f;
+				break;
+			case 2:
+				volume = -12f;
+				break;
+			case 3:
+				volume = -5f;
+				break;
+			case 4:
+				volume = 1f;
+				break;
+			case 5:
+				volume = 6f;
+				break;
 
 		}
 		fc.setValue(volume);
